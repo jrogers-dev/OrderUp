d71778a36e6055a3e3170d7950936a1c53d4183e
Starcatch <timurmishiev@gmail.com>
2021-01-21 18:19:19 -0500

    create dummy data

---
+++ src/sample-menu-items.js
@@ -0,0 +1,41 @@
+const menuItems ={
+  item1: {
+    name: 'Avocado Toast',
+    image: '/images/avocadotoast.jpg',
+    desc:
+      'toasted rye bread with mexican avocado',
+    price: $13,
+    status: 'available'
+  },
+
+  item2: {
+    name: 'Mediterranean Breakfast ',
+    image: '/images/medbreakfast.jpg',
+    desc:
+      'cheese board with olives',
+    price: $15,
+    status: 'available'
+  },
+
+  item3: {
+    name: 'New York Bagel',
+    image: '/images/nycbagel.jpg',
+    desc:
+      ' sesame bagel with cream cheese, and  smoked atlantic salmon',
+    price: $17,
+    status: 'unavailable'
+  },
+
+  item4: {
+    name: 'Pancakes',
+    image: '/images/pancakes.jpg',
+    desc:
+      ' delicious pancakes with mapple syrup and strawberries ',
+    price: $17,
+    status: 'unavailable'
+  }
+
+
+}
+
+export default menuItems
